9e83a584e2d6d60edbdf1490644365f3ab4ff141
Johnson Zhuang <xzhuangaa@connect.ust.hk>
2021-09-28 23:53:09 +0800

    home page block 01 reactive

--- webapp/src/pages/Home/index.tsx
+++ webapp/src/pages/Home/index.tsx
@@ -1,5 +1,31 @@
+import "./index.scss";
+import pichi01 from "../../images/home-pichi01.png";
+import pichi02 from "../../images/home-pichi02.png";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="home">
+      <div className="block block-01">
+        <div className="imgs">
+          <img className="img img-01" src={pichi01} alt="Pichi picture 01" />
+          <img className="img img-02" src={pichi02} alt="Pichi picture 02" />
+        </div>
+        <div className="container">
+          <div className="title-container">
+            <div className="title">
+              <p>Hello Pichi</p>
+            </div>
+            <div className="sub-title">
+              <p>- I'm just a baby catty</p>
+            </div>
+            <div className="btn">
+              <p>About Me</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
